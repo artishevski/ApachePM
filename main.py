599a5049ac2a76fcff2ef4ae3b6cbd261d91d4aa
Aliaksandr Artsisheuski <artsisheuski24@gmail.com>
2022-12-28 12:58:21 +0300

    --\\--

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ from PMProj.View.window import Window
 if __name__ == "__main__":
     data = AccountsInfo()
     window = Window(data)
-    #window.update(data[0])
-    #for i in data:
-    #    i.get_info()
-    #    print()
-
